9ada0d43f548ab94d6955009ce1643d82e6d7cb9
Nick dos Remedios <nick.dosremedios@csiro.au>
2024-12-05 09:41:05 +1100

    Added BIE download test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+const fs = require('fs');
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -21,8 +22,7 @@ test('get started link', async ({ page }) => {
 test.use({ userAgent: 'GH Actions Bot 1.0' });
 
 test('ALA search button test', async ({ page }) => {
-  const baseUrl = 'https://bie.ala.org.au';
-  await page.goto(baseUrl);
+  await page.goto('https://bie.ala.org.au');
 
   // Type text into the search input.
   await page.fill('#search', 'Acacia');
@@ -33,4 +33,34 @@ test('ALA search button test', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Search for Acacia' })).toBeVisible();
 
+});
+
+test('Acacia download test', async ({ page }) => {
+  await page.goto('https://bie.ala.org.au/search?q=Acacia&rows=20');
+
+  // Enable async handling of downloads
+  const downloadPromise = page.waitForEvent('download');
+
+  // Perform the action that initiates the download
+  await page.getByRole('link', { name: ' Download' }).click(); // Unicode character for download icon
+
+  // Wait for download to begin
+  const download = await downloadPromise;
+
+  // Wait for the download process to complete and get the downloaded file path
+  await download.saveAs('/tmp/' + download.suggestedFilename());
+  const filePath = await download.path();
+
+  // Read file contents directly without saving permanently
+  const fileContent = await fs.promises.readFile(filePath, 'utf-8');
+
+  // Verify file contents
+  expect(fileContent).toContain('taxonID');
+  expect(fileContent).toContain('scientificName');
+  expect(fileContent).toContain('APNI');
+  expect(fileContent).toContain('Acacia dealbata');
+
+  // Check file is roughly the expected size (over 3000 lines)
+  const lineCount = fileContent.split('\n').length;
+  expect(lineCount).toBeGreaterThan(3000);
 });
