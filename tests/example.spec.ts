317457721ab2c845c887e452a3769a2871068599
Nick dos Remedios <nick.dosremedios@csiro.au>
2024-12-05 09:11:39 +1100

    Added BIE test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,21 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+// Needed for BIE WAF on GH actions servers
+test.use({ userAgent: 'GH Actions Bot 1.0' });
+
+test('ALA search button test', async ({ page }) => {
+  const baseUrl = 'https://bie.ala.org.au';
+  await page.goto(baseUrl);
+
+  // Type text into the search input.
+  await page.fill('#search', 'Acacia');
+
+  // Click the get started link.
+  await page.getByRole('button', { name: 'Search', exact: true }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Search for Acacia' })).toBeVisible();
+
+});
